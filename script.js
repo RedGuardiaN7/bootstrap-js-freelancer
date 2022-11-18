c696f2eb7cde840065b9e27fb66b6c7a7fdcd01b
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-18 14:14:02 +0100

    Implementata applcazione dei codici sconti

--- script.js
+++ script.js
@@ -29,13 +29,13 @@ function PrintPrice() {
 
     hours = Math.round(hours);
 
-    //Grazie alla funzione first_price si ottiene il primo prezzo (senza sconti) in base alle ore e al tipo di lavoro richiesto
+    //Grazie alla funzione full_price si ottiene il primo prezzo (senza sconti) in base alle ore e al tipo di lavoro richiesto
+    
+    let first_price = full_price (hours);
 
-    console.log(first_price (hours));
+    let final_price = discount (first_price);
 
-   
-
-
+    console.log(final_price)
 }
 
 
@@ -43,17 +43,41 @@ function PrintPrice() {
 
 //Creazione funzione che, leggendo la scelta dell'utente riguardando il tipo di lavoro insieme al numero di ore richieste(hrs), calcola il primo prezzo della commissione (senza sconti)
 
-function first_price (hrs) {
+function full_price (hrs) {
 
     //Lettura del form riguardante il tipo di lavoro richiesto
 
     let type_of_work = document.getElementById("type_of_work").value
 
-    if (type_of_work == 1) {
-        return 15.30 * hrs;
+    if (type_of_work == 1) {                    
+        return 15.30 * hrs;                 // Se la commissione riguarda lo sviluppo frontend (1) il prezzo orario è di 15.30 € l’ora
     } else if (type_of_work == 2) {
-        return 20.50 * hrs;
+        return 20.50 * hrs;                 // Se la commissione riguarda lo sviluppo backend (2) il prezzo orario è di 20.50 € l’ora
     } else if (type_of_work == 3) {
-        return 33.60 * hrs;
+        return 33.60 * hrs;                 // Se la commissione riguarda l’analisi progettuale (3) di un progetto il prezzo orario è di 33.60 € l'ora
+    }
+}
+
+
+//Prima di creare la funzione per l'applicazione dello sconto, è necesario creare un array con i codici sconto accettati
+
+let discount_codes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+//Creazione funzione che calcola il prezzo finale della commissione (applicando eventuali sconti)
+
+function discount (price) {
+
+    //Lettura dell'input dell'utente e trasformazione in variabile
+
+    let input = document.getElementById("formDiscountInput").value;
+
+    input = input.toUpperCase();            // Si trasforma l'input dell'utente in un codice in maiuscolo per verificare se il codice inserito è presente tra quelli consentiti
+
+    if (discount_codes.includes(input)) {                       //Se il codice inserito dall'utente è presente tra quelli accettati, viene applicato il 25% di sconto
+        console.log("Codice sconto applicato! (-25%)");             
+        return price - 0.25 * price;
+    } else {
+        console.log("Codice sconto non inserito/invalido.");    
+        return price;                                           //Altrimenti, non applicando alcun sconto, il prezzo finale equivale al prezzo iniziale
     }
 }
